eb1379f6f866c9a8d65ed69147ae93fffc2d0198
erikroos <tom.erik.roos@gmail.com>
2011-04-13 20:18:02 +0000

    BootStrap now creates (if not yet existent) a test Ontology, Investigation Template, Animal Template, Investigation and Species (Term), so you can test the application more easily, without having to add all these first.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,8 +1,32 @@
+import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.dbnp.gdt.*
 import nl.nbic.animaldb.*
 
 class BootStrap {
 	def init = { servletContext ->
+
+                // developmental/test bootstrapping:
+		//      - templates
+		//      - ontologies
+		//      - and/or investigations
+		if (    grails.util.GrailsUtil.environment == GrailsApplication.ENV_DEVELOPMENT ||
+                grails.util.GrailsUtil.environment == GrailsApplication.ENV_TEST) {
+			// add ontologies?
+			def testOntology = Ontology.getOrCreateOntologyByNcboId(1132)
+
+			// add templates?
+			if (!Template.count()) {
+                            new Template(name: 'testTemplateInv', entity: Investigation).save(failOnError: true)
+                            new Template(name: 'testTemplateAn', entity: Animal).save(failOnError: true)
+                        }
+
+			// add example investigation?
+			if (!Investigation.count()) new Investigation(name: 'testInvestigation').save(failOnError: true)
+
+                        // add species?
+                        if (!Term.count()) Term.getOrCreateTerm('Mesocricetus auratus', testOntology, 'blaat')
+		}
+
 		/**
 		 * attach ontologies in runtime. Possible problem is that you need
 		 * an internet connection when bootstrapping though.
