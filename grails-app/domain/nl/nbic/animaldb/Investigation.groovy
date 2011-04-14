74b37c198584868960485882b26c490b57f48bf8
4np <work@osx.eu>
2011-04-14 12:12:10 +0000

    - made animals appears in the same order instead of random (default)

--- grails-app/domain/nl/nbic/animaldb/Investigation.groovy
+++ grails-app/domain/nl/nbic/animaldb/Investigation.groovy
@@ -3,28 +3,28 @@ package nl.nbic.animaldb
 import org.dbnp.gdt.*
 
 class Investigation extends TemplateEntity {
-
 	String name			// the name of an investigation
+	List animals		// keep animals in order
 
 	// an investigation is done over many animals
 	static hasMany = [
 		animals: Animal
-	] 
+	]
 
 	static constraints = {
-		name(nullable:false, blank: false, maxSize: 255)
+		name(nullable: false, blank: false, maxSize: 255)
 	}
 
-
+	/**
+	 * Delete an animal from the investigation
+	 * @param animal
+	 */
 	void deleteAnimal(Animal animal) {
-
 		// This should remove the animal itself too, because of the cascading belongsTo relation
 		this.removeFromAnimals(animal)
 
 		// But apparently it needs an explicit delete() too
 		animal.delete()
-
-
 	}
 
 	/**
@@ -44,17 +44,17 @@ class Investigation extends TemplateEntity {
 
 	static final List<TemplateField> domainFields = [
 		new TemplateField(
-		    name: 'name',
-		    type: TemplateFieldType.STRING,
-		    comment: 'an investigation should have a name describing the investigation',
-		    required: true)
+			name: 'name',
+			type: TemplateFieldType.STRING,
+			comment: 'an investigation should have a name describing the investigation',
+			required: true)
 	]
 
-    /**
-     *
-     * @return name representation of the investigation
-     */
-    String toString() {
-        name
-    }
+	/**
+	 *
+	 * @return name representation of the investigation
+	 */
+	String toString() {
+		name
+	}
 }
